bd5ac5ff9540ffad32bf799f7e8670d0a7cfe4cb
fajrCode <fajri42vandi@gmail.com>
2024-06-26 15:21:00 +0700

    model: set relation

--- app/Models/Absen.php
+++ app/Models/Absen.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Absen extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_karyawan',
+        'bukti',
+        'lokasi',
+        'status',
+    ];
+
+    public function getHashidAttribute()
+    {
+        return Hashids::encode($this->id);
+    }
+
+    public function karyawan(): BelongsTo
+    {
+        return $this->belongsTo(Karyawan::class, 'id_karyawan');
+    }
+
 }
--- app/Models/Izin.php
+++ app/Models/Izin.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Izin extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_karyawan',
+        'keterangan',
+        'alasan',
+        'mulai',
+        'selesai',
+        'status',
+    ];
+
+    public function getHashidAttribute()
+    {
+        return Hashids::encode($this->id);
+    }
+
+    public function karyawan(): BelongsTo
+    {
+        return $this->belongsTo(Karyawan::class, 'id_karyawan');
+    }
+
 }
