cde850fa2a1f96444dc0e8156d84fa96b844a967
fajrCode <fajri42vandi@gmail.com>
2024-06-30 22:30:08 +0700

    fix: register controller

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,11 +1,11 @@
 @props(['messages'])
 
 @if ($messages)
-    <button type="button" class="btn btn-link text-danger tooltip-btn invalid-tooltip"
+    {{-- <button type="button" class="btn btn-link text-danger tooltip-btn invalid-tooltip"
     data-bs-toggle="tooltip" data-bs-placement="left">
-    <i class="bi bi-exclamation-circle"></i>
+    <i class="bi bi-exclamation-circle"></i> --}}
     </button>
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-white text-left fst-italic']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -6,16 +6,6 @@
                 <form method="POST" action="{{ route('register') }}" class="was-validated">
                     @csrf
                     <div class="form-floating mb-3">
-                        <x-text-input type="text" id="nik" name="nik" placeholder="Masukan NIK" :value="old('nik')" required autofocus/>
-                        <x-input-label for="nik" :value="__('NIK')" />
-                        <x-input-error :messages="$errors->get('nik')" class="mt-2" />
-                    </div>
-                    <div class="form-floating mb-3">
-                        <x-text-input type="text" id="name" name="name" placeholder="Masukan Nama" :value="old('name')" required autocomplete="name" />
-                        <x-input-label for="name" :value="__('Name')" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                    </div>
-                    <div class="form-floating mb-3">
                         <x-text-input type="email" id="emailaddress" name="email" placeholder="Masukan Email" :value="old('email')" required autocomplete="username" />
                         <x-input-label for="emailaddress" :value="__('Email')" />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -23,11 +13,13 @@
                     <div class="form-floating mb-3">
                         <x-text-input type="password" id="password" name="password" placeholder="Masukan Password" required autocomplete="new-password" />
                         <x-input-label for="password" :value="__('Password')" />
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
                     <div class="form-floating mb-3">
-                        <x-text-input type="password" id="password" name="password" placeholder="Masukan Konfirmasi Password" required autocomplete="new-password" />
+                        <x-text-input type="password" id="password_confirmation" name="password_confirmation" placeholder="Masukan Konfirmasi Password" required autocomplete="new-password" />
                         <x-input-label for="password" :value="__('Confirm Password')" />
+                    </div>
+                    <div class="form-floating mb-3">
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                     </div>
                     <div class="d-grid">
