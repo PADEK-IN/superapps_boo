9de044861db9f10d7e23ff6e76ca3f9e2de91250
MRoyhanF <froyhan0@gmail.com>
2024-07-01 23:37:38 +0700

    setup page admin from sidebar

---
+++ resources/views/pages/admin/karyawan/list.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus aliquid alias quia odit dolorem, nihil accusamus nam soluta consequatur placeat sit ut culpa molestiae quo, eligendi itaque in doloremque.</p>
+</x-admin-layout>
--- routes/admin.php
+++ routes/admin.php
@@ -3,10 +3,10 @@
 use App\Http\Middleware\isValidAdmin;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminIndexController;
-use App\Http\Controllers\AdminAbsenController;
-use App\Http\Controllers\AdminIzinController;
-use App\Http\Controllers\AdminKaryawanController;
-use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\Admin\AdminAbsenController;
+use App\Http\Controllers\Admin\AdminIzinController;
+use App\Http\Controllers\Admin\AdminKaryawanController;
+use App\Http\Controllers\Admin\AdminUserController;
 
 Route::prefix('admin')->middleware(['auth', 'verified', isValidAdmin::class])->group(function () {
     Route::get('/dashboard', [AdminIndexController::class, 'index'])->name('admin.dashboard');
