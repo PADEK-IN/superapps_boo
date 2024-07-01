806791c0bfd893a1cb89df6c63e7b2e8c162d250
fajrCode <fajri42vandi@gmail.com>
2024-07-01 23:04:37 +0700

    fix: auth session controller

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,16 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = Auth::user();
+
+        if ($user->role==="admin") {
+            return redirect()->intended(route('admin.dashboard', absolute: false));
+        } elseif ($user->role==="karyawan") {
+            return redirect()->intended(route('dashboard', absolute: false));
+        } else {
+            return redirect()->intended(route('login', absolute: false));
+        }
+
     }
 
     /**
