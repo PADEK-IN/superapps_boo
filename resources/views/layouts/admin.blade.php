79cdc6e44fb65dade20c097eac813b4c6bec67f7
MRoyhanF <froyhan0@gmail.com>
2024-07-02 00:54:55 +0700

    slicing lisu karyawan page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,12 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>{{ config('app.name', 'Super Apps PT.APM Logistics') }}</title>
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/favicon180.png') }}" sizes="180x180">
+    <link rel="icon" href="{{ asset('assets/img/favicon32.png') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ asset('assets/img/favicon16.png') }}" sizes="16x16" type="image/png">
+
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{ asset('assets/admin/css/styles.css') }}" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
--- resources/views/pages/admin/karyawan/list.blade.php
+++ resources/views/pages/admin/karyawan/list.blade.php
@@ -1,3 +1,66 @@
 <x-admin-layout>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus aliquid alias quia odit dolorem, nihil accusamus nam soluta consequatur placeat sit ut culpa molestiae quo, eligendi itaque in doloremque.</p>
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">List Semua Karyawan</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="/admin/dashboard">Dashboard</a></li>
+                <li class="breadcrumb-item active">List Karyawan</li>
+            </ol>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    Tabel Karyawan
+                </div>
+                <div class="card-body text-center">
+                    <table id="datatablesSimple" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>NIK</th>
+                                <th>Nama</th>
+                                <th>Jenis Kelamin</th>
+                                <th>No HP</th>
+                                <th>Jabatan</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>4267384249</td>
+                                <td>Joko Widodo</td>
+                                <td>
+                                    <span class="badge bg-primary">Laki-laki</span>
+                                </td>
+                                <td>08263728123</td>
+                                <td>Direktur</td>
+                                <td>
+                                    <span class="badge bg-success">Active</span>
+                                </td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>4267384249</td>
+                                <td>Putri Wulandari</td>
+                                <td>
+                                    <span class="badge bg-warning">Perempuan</span>
+                                </td>
+                                <td>08263728123</td>
+                                <td>Direktur</td>
+                                <td>
+                                    <span class="badge bg-success">Active</span>
+                                </td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
 </x-admin-layout>
