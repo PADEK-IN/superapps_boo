3695904e8ac402df7cef0b0632d78543552a1f61
fajrCode <fajri42vandi@gmail.com>
2024-07-03 01:29:28 +0700

    feat: absen karyawan adjust | image din't upload

--- app/Http/Controllers/Karyawan/AbsenController.php
+++ app/Http/Controllers/Karyawan/AbsenController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Karyawan;
 
 use App\Models\Absen;
+use App\Models\Karyawan;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AbsenController extends Controller
 {
@@ -15,33 +17,35 @@ class AbsenController extends Controller
 
     public function createPage()
     {
-        return view('pages.karyawan.absen.create');
+        $id_user = Auth::id();
+        $nama = Karyawan::where('id_user', $id_user)->firstOrFail()->nama;
+        return view('pages.karyawan.absen.create', compact('nama'));
     }
 
     public function store(Request $request)
     {
-        $request->validate([
-            'id_karyawan' => 'required',
-            'bukti' => 'required',
-            'latitude' => 'required',
-            'longitude' => 'required',
-            'jarak' => 'required',
-            'date' => 'required|date',
-            'time' => 'required|date_format:H:i',
-            'status' => 'required',
-        ]);
+        try {
+            $request->validate([
+                // 'bukti' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'latitude' => 'required|string|max:20',
+                'longitude' => 'required|string|max:20',
+                'jarak' => 'required|string|max:20',
+                'waktu' => 'required',
+            ]);
 
-        $absen = Absen::create([
-            'id_karyawan' => $request->id_karyawan,
-            'bukti' => $request->bukti,
-            'latitude' => $request->latitude,
-            'longitude' => $request->longitude,
-            'jarak' => $request->jarak,
-            'date' => $request->date,
-            'time' => $request->time,
-            'status' => $request->status
-        ]);
+            $id_user = Auth::id();
+            $karyawan = Karyawan::where('id_user', $id_user)->firstOrFail();
+            $id_karyawan = $karyawan->id;
 
-        return redirect()->route('pages.karyawan.absen.list');
+            // Gabungkan data request dengan ID karyawan
+            $data = array_merge($request->all(), ['id_karyawan' => $id_karyawan]);
+
+            // Absen::create($data);
+
+            return redirect()->route('pages.karyawan.absen.list')->with('status', $data);
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
     }
 }
