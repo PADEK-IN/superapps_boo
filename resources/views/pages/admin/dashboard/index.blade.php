0e7fe63f3251ecc7b3b0415598a120d7a870afd2
MRoyhanF <froyhan0@gmail.com>
2024-07-04 13:07:10 +0700

    featch detail absensi and setup dashboad admin page

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -8,11 +8,36 @@
             <div class="row">
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-primary text-white mb-4">
-                        <div class="card-body">Primary Card</div>
+                        <div class="card-body"><b>Karyawan Aktif</b></div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            {{-- <a class="small text-white stretched-link" href="#">View Details</a> --}}
-                            <a class="small text-white stretched-link">View Details</a>
-                            {{-- <div class="small text-white"><i class="fas fa-angle-right"></i></div> --}}
+                            <a class="small text-white stretched-link">Total</a>
+                            <div class="small text-white">
+                                sekian
+                                <i class="fas fa-user fa-fw"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-success text-white mb-4">
+                        <div class="card-body"><b>Karyawan Izin</b></div>
+                        <div class="card-footer d-flex align-items-center justify-content-between">
+                            <a class="small text-white stretched-link">Total</a>
+                            <div class="small text-white">
+                                <i class="fas fa-user fa-fw"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-warning text-white mb-4">
+                        <div class="card-body"><b>Karyawan Sakit</b></div>
+                        <div class="card-footer d-flex align-items-center justify-content-between">
+                            <a class="small text-white stretched-link">Total</a>
+                            <div class="small text-white">
+                                sekian
+                                <i class="fas fa-user fa-fw"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
