52532e85739185d43e38901b5795648f7f54a0cb
MRoyhanF <froyhan0@gmail.com>
2024-07-09 20:58:34 +0700

    fix bug at user pedit

--- routes/karyawan.php
+++ routes/karyawan.php
@@ -20,7 +20,7 @@ Route::middleware(['auth', 'verified', isValidKaryawan::class])->group(function 
     Route::get('/izin/create', [IzinController::class, 'createPage'])->name('izin.create');
     Route::post('/izin/store', [IzinController::class, 'storeData'])->name('izin.store');
 
-    Route::patch('/profile/password/update', [ProfileController::class, 'password-update'])->name('profile.password.update');
+    Route::patch('/profile/update', [ProfileController::class, 'update'])->name('profile.password.update');
     Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 
